066cd77316fb8a6cdbb421f2a893b8132e327349
Piotr Wolny <dacekonn@gmail.com>
2022-06-01 14:59:58 +0000

    dispaly a simple rectangle

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,6 @@ fn main() {
 fn add_systems(app : &mut App) {
     app
         .add_plugins(DefaultPlugins)
-        .add_system(hello_world::hello_world);
+        .add_system(hello_world::hello_world)
+        .add_system(simple_rectangle::setup);
 }
