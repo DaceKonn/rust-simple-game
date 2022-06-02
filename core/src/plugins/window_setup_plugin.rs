89d1d1f90f01b1068ecbfef7e6b443e3971a7c51
DaceKonn <dacekonn@gmail.com>
2022-06-02 17:09:40 +0200

    - organized code into plugins - added basic window setup

---
+++ core/src/plugins/window_setup_plugin.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+pub struct WindowSetupPlugin {
+    height: f32
+}
+
+impl Default for WindowSetupPlugin {
+    fn default() -> Self {
+        Self { height: 900.0 }
+    }
+}
+
+impl Plugin for WindowSetupPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(ClearColor(CLEAR))
+            .insert_resource(WindowDescriptor {
+                width: self.height * RESOLUTION,
+                height: self.height,
+                title: "Bevy Tutorial".to_string(),
+                resizable: false,
+                ..Default::default()
+            });
+    }
+}
